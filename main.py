8ff8f5392c1cc2f25cca6fff530b84076b3328e7
Marline Khavele <khavelemarline@gmail.com>
2025-02-21 13:53:22 +0100

    add render support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import api_router
@@ -24,4 +25,7 @@ app.include_router(api_router, prefix="/api")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))  # Use Render's assigned port
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
+    
